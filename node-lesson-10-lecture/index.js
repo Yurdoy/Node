08f57837c5d0a41ea96140537fd15a54e791bf1e
Yurdoy <yurdoy@gmail.com>
2025-07-07 10:29:35 +0200

    Complete working with lecture

--- node-lesson-10-lecture/index.js
+++ node-lesson-10-lecture/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
-import jws from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 const app = express();
@@ -51,6 +51,64 @@ app.post("/login", async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
+});
+
+function authenticateJWT(req, res, next) {
+  const authHeader = req.header.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ message: "Forbidden: Invalid or expired token" });
+      }
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json({ message: "Unauthorized: No token povided" });
+  }
+}
+
+app.get("/profile", authenticateJWT, (req, res) => {
+  res.json({
+    id: req.user.id,
+    username: req.user.username,
+    role: req.user.role,
+    message: "Here is your profile information",
+  });
+});
+
+function authorizeRole(role) {
+  return (req, res, next) => {
+    if (req.user.role === role) {
+      next();
+    } else {
+      return res
+        .status(403)
+        .json({
+          message: "Forbidden: You do not have access to this resource",
+        });
+    }
+  };
+}
+
+app.get("/me", authenticateJWT, (req, res) => {
+  res.json({
+    id: req.user.id,
+    username: req.user.username,
+    role: req.user.role,
+    message: "Here is your personal information",
+  });
+});
+
+app.get("/admin", authenticateJWT, authorizeRole("admin"), (req, res) => {
+  res.json({
+    message: "Welcome, Admin! You have access to this protected route.",
+  });
 });
 
 const PORT = process.env.PORT || 3000;
